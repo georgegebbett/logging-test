0053c5592ef6ed5945aad6135605e85e848aaaa5
George Gebbett <george.gebbett1@gmail.com>
2024-07-27 14:50:24 +0100

    no lint on biuld

--- next.config.js
+++ next.config.js
@@ -6,18 +6,22 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
+    reactStrictMode: true,
 
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-  transpilePackages: ["geist"],
+    /**
+     * If you are using `appDir` then you must comment the below `i18n` config out.
+     *
+     * @see https://github.com/vercel/next.js/issues/41980
+     */
+    i18n: {
+        locales: ["en"],
+        defaultLocale: "en",
+    },
+    transpilePackages: ["geist"],
+    eslint: {
+        ignoreDuringBuilds: true
+    }
+
 };
 
 export default config;
